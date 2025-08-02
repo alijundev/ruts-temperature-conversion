3a326214eb34ad66c5158d95cbcc693df3204aaf
alijundev <alijun.dev@gmail.com>
2025-08-02 22:57:55 +0800

    feat(conversion): implement konversion from celcius to fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let celcius: i32 = 36;
+
+    let fahrenheit = ( 9 / 5 * celcius) + 32;
+
+    println!("Suhu dalam celcius: {celcius}");
+    println!("Suhu dalam fahrenheit: {fahrenheit}");
 }
